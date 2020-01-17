87161e27782f4f3510b0fcf99795976c5de9e3a8
Michael <contact@eewee.fr>
2020-01-17 12:01:20 +0100

    BWD3 - example CV (use : variable, array, boucle, form).

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+$prenom = "Thomas";
+$nom = "DOE";
+$email = "johndoe@tld.com";
+$age = 20;
+$tel = "06 23 45 67 89";
+$adresse = "140, City Center, New York, U.S.A";
+$langues = ["English", "German", "French"];
+
+$workExperience = [
+    [
+        "dateDebut"   => "MARS 2020",
+        "dateFin"     => "Present",
+        "societe"     => "Xxx",
+        "titre"       => "mon titre 1",
+        "description" => "Bla bla",
+    ],
+    [
+        "dateDebut"   => "JUILLET 2018",
+        "dateFin"     => "MARS 2020",
+        "societe"     => "Yyy",
+        "titre"       => "mon titre 2",
+        "description" => "Bla bla 2",
+    ]
+];
