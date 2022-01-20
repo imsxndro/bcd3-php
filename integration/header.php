5623a71f7fb97626ed798603337ab89a1817547e
imsxndro <73113443+imsxndro@users.noreply.github.com>
2022-01-20 17:00:18 +0100

    Update header.php

--- integration/header.php
+++ integration/header.php
@@ -31,14 +31,14 @@
         <div class="col-12">
 
           <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="">TP</a>
+            <a class="navbar-brand" href="/">TP</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
               <ul class="navbar-nav">
                 <li class="nav-item active">
-                  <a class="nav-link" href="">Google Font <span class="sr-only"></span></a>
+                  <a class="nav-link" href="/">Google Font <span class="sr-only"></span></a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="font-awesome.php">Font Awesome</a>
