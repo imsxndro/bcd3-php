a66dccb7adfc5383e6e9a219831a0a881af80ab4
imsxndro <73113443+imsxndro@users.noreply.github.com>
2022-01-21 15:06:58 +0100

    Update config.php

--- cv/config.php
+++ cv/config.php
@@ -8,28 +8,28 @@ $tel = "01 23 45 67 89";
 $adresse = "27 Rue Enrico Fermi";
 $langues = ["Français", "Anglais"];
 $reseaux = [
-    "facebook"  => "https://fr-fr.facebook.com/RedBull/",
-    "twitter"   => "https://twitter.com/redbull",
-    "linkedin"  => "https://fr.linkedin.com/company/red-bull",
-    "instagram" => "https://www.instagram.com/redbull/",
+    "facebook"  => "https://fr-fr.facebook.com/",
+    "twitter"   => "https://twitter.com/",
+    "linkedin"  => "https://fr.linkedin.com/",
+    "instagram" => "https://www.instagram.com/",
 ];
 
 $workExperience = [
 
     [
-        "dateDebut"   => "MARS 2021",
-        "dateFin"     => "Present",
-        "societe"     => "Xxx",
-        "titre"       => "Integrateur web",
+        "dateDebut"   => "SEPTEMBRE 2021",
+        "dateFin"     => "Présent",
+        "societe"     => "CGR",
+        "titre"       => "Designer graphique",
         "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt efficitur dignissim. Vestibulum leo est, molestie in ipsum eget, viverra malesuada dui. Integer vitae libero ante. Aenean tempor elementum metus, id bibendum tellus. Aenean consequat purus odio, id condimentum arcu bibendum vitae. Nullam cursus ultricies magna in faucibus. Maecenas ipsum justo, lacinia id mollis vel, dictum at libero. Suspendisse sem eros, vehicula in nisi rutrum, lacinia luctus risus. Morbi id lorem finibus, tincidunt risus vitae, vestibulum neque.",
     ],
 
     [
-        "dateDebut"   => "JUILLET 2018",
-        "dateFin"     => "MARS 2021",
-        "societe"     => "Yyy",
-        "titre"       => "Integrateur web",
-        "description" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras placerat a diam in iaculis. Aliquam erat volutpat. Ut mattis velit sit amet risus maximus, in porta ex porttitor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras et nunc felis. Cras ut ligula metus. Praesent massa orci, facilisis id risus sed, facilisis consectetur mi. Suspendisse tortor velit, venenatis a ex ac, elementum finibus purus. Suspendisse molestie posuere lectus a ornare. Integer neque augue, eleifend eu risus eget, fermentum pulvinar orci.",
+        "dateDebut"   => "AVRIL 2021",
+        "dateFin"     => "AOÛT 2021",
+        "societe"     => "Hôtel Atlantis Royan",
+        "titre"       => "Community Manager",
+        "description" => "Création des réseaux sociaux et mise en place d’une communication personnalisée suivi de création graphique (posts, photos, vidéos) & de l'optimisation SEO du site de l’hôtel",
     ],
 
 ];
