644dd7692d1e6068a25a965a613d541849bb9bcf
imsxndro <73113443+imsxndro@users.noreply.github.com>
2022-02-17 15:52:08 +0100

    Update config.php

--- parc-naturel/config.php
+++ parc-naturel/config.php
@@ -12,17 +12,17 @@ $menu = [
 
 $incontournables = [
     [
-        "image"         => "images/placeholder.png",
+        "image"         => "images/glacier-ga0a01878a_640.jpg",
         "titre"         => "La forêt de Séquoia",
         "description"   => "La plus grande densité au monde d'arbres millénaires, des arbres de plus de 40 mètres de haut",
     ],
     [
-        "image"         => "images/placeholder.png",
+        "image"         => "images/nature-gda19b88ec_640.jpg",
         "titre"         => "Les Gorges du Dardet",
         "description"   => "Au coeur de paysages lunaires, le Dardet a creusé son lit spectaculaire",
     ],
     [
-        "image"         => "images/placeholder.png",
+        "image"         => "images/road-g53d3b1c26_640.jpg",
         "titre"         => "Le glacier de Briefane",
         "description"   => "Serpentant au fond de la vallée de la Briefane, le glacier haut de 15 mètres s'offre majestueusement au regard",
     ],
