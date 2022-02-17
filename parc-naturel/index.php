556b802b5460c12e58bc464f0d1caffdd76ddcb6
imsxndro <73113443+imsxndro@users.noreply.github.com>
2022-02-17 14:02:46 +0100

    create header.php
    hope it's works...

--- parc-naturel/index.php
+++ parc-naturel/index.php
@@ -12,30 +12,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Montserrat:wght@400;600;700;900&display=swap" rel="stylesheet"> 
     </head>
     <body class="container">
-        <!-- En-tête du site -->
-        <header class="display-flex">
-            <!-- Logo -->
-            <a href="#"><img src="images/logo.png" /></a>
-            <div class="menu-contain">
-                <!-- Bloc compte -->
-                <div>
-                    <a href="#"><img src="images/compte.png" /></a>
-                    <a href="#"><img src="images/shopping-basket.png" /></a>
-                </div>
-                <!-- Menu principal -->
-                <nav>
-                    <ul class="display-flex">
-                        <li><a href="#">Accueil</a></li>
-                        <li><a href="#">A propos</a></li>
-                        <li><a href="#">Galerie</a></li>
-                        <li><a href="#">Pratique</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">Shop</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+    <?php require_once "header.php"; ?>
         <!-- Contenu principal-->
         <main>
             <!-- Présentation -->
