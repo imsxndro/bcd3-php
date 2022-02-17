75df99717e4118c4f87cb574d3863f0325a86503
imsxndro <73113443+imsxndro@users.noreply.github.com>
2022-02-17 14:06:30 +0100

    create footer.php
    i'm a senior php dev yeah yeah no joke

---
+++ parc-naturel/footer.php
@@ -0,0 +1,6 @@
+<!-- Pied de page -->
+<footer>            
+            <p><a href="#">Mentions légales</a> - <a href="#">Plan du site</a> - <a href="#">Contact</a> - Copyright 2022 Parc naturel</p>
+        </footer>
+    </body>
+</html>
--- parc-naturel/index.php
+++ parc-naturel/index.php
@@ -96,9 +96,4 @@
                 </div>
             </section>
         </main>
-        <!-- Pied de page -->
-        <footer>            
-            <p><a href="#">Mentions légales</a> - <a href="#">Plan du site</a> - <a href="#">Contact</a> - Copyright 2022 Parc naturel</p>
-        </footer>
-    </body>
-</html>
+        <?php require_once "footer.php"; ?>   
