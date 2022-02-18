c4074f689a67b78871e09b268053665b510cde00
imsxndro <73113443+imsxndro@users.noreply.github.com>
2022-02-18 16:56:35 +0100

    Update contact.php

--- parc-naturel/contact.php
+++ parc-naturel/contact.php
@@ -13,6 +13,19 @@
             </section>
             <!-- Contact -->
                 <?php
+                $con = mysqli_connect('localhost', 'root', '',’db_contact’);
+
+                $txtName = $_POST['your-name'];
+                $txtEmail = $_POST['your-email'];
+                $txtSubject = $_POST['your-subject'];
+                $txtMessage = $_POST['your-message'];
+
+                $con = mysqli_connect('localhost', 'root', '',’db_contact’);
+                $sql = "INSERT INTO `tbl_contact` (`Id`, `fldName`, `fldEmail`, `fldPhone`, `fldMessage`) VALUES ('0', '$txtName', '$txtEmail', '$txtSubject', '$txtMessage');"
+                $rs = mysqli_query($con, $sql);
+
+                
+
                     if (isset($_POST) && !empty($_POST)) {
                         echo "
                         <div class='alert alert-warning' role='alert'>
@@ -21,6 +34,7 @@
                         
                         $contenuEmail = "
                         Nom : ".$_POST['your-name']."
+                        Suket : ".$_POST['your-subject']."
                         Message : ".$_POST['your-message']."
                         Email : ".$_POST['your-email']."
                         ";
